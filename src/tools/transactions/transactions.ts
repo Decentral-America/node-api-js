1da5c6cb93e9c56778ffd744f78e217a731bb2fd
veserion <lobanovdanil97@gmail.com>
2021-10-28 14:40:50 +0300

    fix stateUpdate

--- src/tools/transactions/transactions.ts
+++ src/tools/transactions/transactions.ts
@@ -1,5 +1,5 @@
-import {IWithStateChanges, TPayment, TStateChanges} from "../../api-node/debug";
-import {BigNumber} from "@waves/bignumber";
+import { IWithStateChanges, TPayment, TStateChanges } from "../../api-node/debug";
+import { BigNumber } from "@waves/bignumber";
 import {
     AssetDecimals,
     DataTransactionEntry,
@@ -8,7 +8,7 @@ import {
     TRANSACTION_TYPE,
     WithApiMixin
 } from "@waves/ts-types/";
-import {Long} from "@waves/ts-types/";
+import { Long } from "@waves/ts-types/";
 import {
     AliasTransaction,
     BurnTransaction, CancelLeaseTransaction, DataTransaction, ExchangeTransaction,
@@ -17,7 +17,7 @@ import {
     PaymentTransaction, ReissueTransaction, SetAssetScriptTransaction, SetScriptTransaction, SponsorshipTransaction,
     TransferTransaction, UpdateAssetInfoTransaction
 } from "@waves/ts-types/";
-import {IWithApplicationStatus, TLong} from "../../interface";
+import { IWithApplicationStatus, TLong } from "../../interface";
 import any = jasmine.any;
 
 export type TStateUpdate = {
@@ -91,26 +91,35 @@ export type TTransaction<LONG = Long> =
 
 
 export function addStateUpdateField(transaction: TTransaction & WithApiMixin & IWithApplicationStatus): TTransaction & WithApiMixin & IWithApplicationStatus {
-    if (transaction.type === TRANSACTION_TYPE.INVOKE_SCRIPT || transaction.type === TRANSACTION_TYPE.INVOKE_EXPRESSION && transaction.stateChanges.invokes && transaction.stateChanges.invokes.length) {
+    if (transaction.type === TRANSACTION_TYPE.INVOKE_SCRIPT && transaction.stateChanges.invokes.length) {
         const payments = (transaction as any).payment ? (transaction as any).payment.map((p: TPayment) => ({
             assetId: p.assetId,
             amount: p.amount
         })) : []
 
-     } if (transaction.type === TRANSACTION_TYPE.ETHEREUM && transaction.payload.type === 'invocation' && transaction.payload.stateChanges.invokes && transaction.payload.stateChanges.invokes.length) {
+        return Object.defineProperty(transaction, 'stateUpdate', { get: () => makeStateUpdate(transaction.stateChanges, payments, transaction.dApp, transaction.sender) })
+    } if (transaction.type === TRANSACTION_TYPE.INVOKE_EXPRESSION && transaction.stateChanges.invokes && transaction.stateChanges.invokes.length) {
+        const payments = (transaction as any).payment ? (transaction as any).payment.map((p: TPayment) => ({
+            assetId: p.assetId,
+            amount: p.amount
+        })) : []
+
+        const dApp = ""
+        return Object.defineProperty(transaction, 'stateUpdate', { get: () => makeStateUpdate(transaction.stateChanges, payments, dApp, transaction.sender) })
+    } if (transaction.type === TRANSACTION_TYPE.ETHEREUM && transaction.payload.type === 'invocation' && transaction.payload.stateChanges.invokes && transaction.payload.stateChanges.invokes.length) {
         const payments = transaction.payload.payment ? transaction.payload.payment.map((p: TPayment) => ({
             assetId: p.assetId,
             amount: p.amount
         })) : []
         const dApp = transaction.payload.dApp || ''
         // @ts-ignore
-        return Object.defineProperty(transaction, 'stateUpdate', {get: () => makeStateUpdate(transaction.payload.stateChanges, payments, dApp, transaction.sender)})
+        return Object.defineProperty(transaction, 'stateUpdate', { get: () => makeStateUpdate(transaction.payload.stateChanges, payments, dApp, transaction.sender) })
     } else return transaction
 }
 
 export function makeStateUpdate(stateChanges: TStateChanges, payment: TPayment[], dApp: string | undefined, sender: string): TStateUpdate {
-    const payments = payment.map(payment => ({payment, dApp, sender}))
-    const addField = (array: any[], fieldName: string) => array.map(item => ({...item, [fieldName]: dApp}))
+    const payments = payment.map(payment => ({ payment, dApp, sender }))
+    const addField = (array: any[], fieldName: string) => array.map(item => ({ ...item, [fieldName]: dApp }))
     const transfers = addField(stateChanges.transfers, 'sender')
     const leases = addField(stateChanges.leases, 'sender')
     const issues = addField(stateChanges.issues, 'address')
@@ -135,70 +144,70 @@ export function makeStateUpdate(stateChanges: TStateChanges, payment: TPayment[]
     const recursiveFunction = (stateChanges: TStateChanges, sender: string) => {
         if (stateChanges.invokes.length) {
             stateChanges.invokes.forEach((x) => {
-                    //payments
-                    if (x.payment) x.payment.forEach(y => {
-                        const index = payments.findIndex(z => (z.payment.assetId === y.assetId) && (z.dApp === x.dApp) && (sender === x.dApp))
-                        index !== -1
-                            ? payments[index].payment.amount = (new BigNumber(payments[index].payment.amount)).add(y.amount).toNumber()
-                            : payments.push({
-                                payment: y,
-                                sender: sender,
-                                dApp: x.dApp
-                            })
-                    })
-                    //data
-                    x.stateChanges.data.forEach(y => {
-                        const index = stateUpdate.data.findIndex(z => z.key === y.key && z.address === x.dApp)
-                        index !== -1 ? stateUpdate.data[index] = {...y, address: x.dApp} : stateUpdate.data.push({
-                            ...y,
-                            address: x.dApp
+                //payments
+                if (x.payment) x.payment.forEach(y => {
+                    const index = payments.findIndex(z => (z.payment.assetId === y.assetId) && (z.dApp === x.dApp) && (sender === x.dApp))
+                    index !== -1
+                        ? payments[index].payment.amount = (new BigNumber(payments[index].payment.amount)).add(y.amount).toNumber()
+                        : payments.push({
+                            payment: y,
+                            sender: sender,
+                            dApp: x.dApp
                         })
+                })
+                //data
+                x.stateChanges.data.forEach(y => {
+                    const index = stateUpdate.data.findIndex(z => z.key === y.key && z.address === x.dApp)
+                    index !== -1 ? stateUpdate.data[index] = { ...y, address: x.dApp } : stateUpdate.data.push({
+                        ...y,
+                        address: x.dApp
                     })
-                    //burns
-                    x.stateChanges.burns.forEach(y => {
-                            const index = stateUpdate.burns.findIndex(z => z.assetId === y.assetId)
-                            index !== -1 ? stateUpdate.burns[index].quantity += y.quantity : stateUpdate.burns.push({
-                                ...y,
-                                address: x.dApp
-                            })
-                        }
-                    )
-                    //issues
-                    x.stateChanges.issues.forEach(y => stateUpdate.issues.push({...y, address: x.dApp}))
-                    //reissues
-                    x.stateChanges.reissues.forEach(y => {
-                            const index = stateUpdate.reissues.findIndex(z => z.assetId === y.assetId)
-                            index !== -1 ? stateUpdate.reissues[index].quantity += y.quantity : stateUpdate.reissues.push({
-                                ...y,
-                                address: x.dApp
-                            })
-                        }
-                    )
-                    //transfers
-                    x.stateChanges.transfers.forEach(y => {
-                            const index = stateUpdate.transfers.findIndex(z => (z.asset === y.asset) && (z.address === y.address) && (x.dApp === z.sender))
-                            index !== -1
-                                ? stateUpdate.transfers[index].amount = (new BigNumber(stateUpdate.transfers[index].amount)).add(y.amount).toNumber()
-                                : stateUpdate.transfers.push({
-                                    ...y,
-                                    sender: x.dApp
-                                })
-                        }
-                    )
-                    //sponsorFees
-                    x.stateChanges.sponsorFees.forEach(y => {
-                            const index = stateUpdate.sponsorFees.findIndex(z => (z.assetId === y.assetId) && (z.address === x.dApp))
-                            index !== -1
-                                ? stateUpdate.sponsorFees[index] = {...y, address: x.dApp}
-                                : stateUpdate.sponsorFees.push({...y, address: x.dApp})
-                        }
-                    )
-                    //lease and leaseCancels
-                    x.stateChanges.leases.forEach(y => stateUpdate.leases.push({...y, sender: x.dApp}))
-                    x.stateChanges.leaseCancels.forEach(y => stateUpdate.leaseCancels.push({...y, address: x.dApp}))
-
-                    recursiveFunction(x.stateChanges, x.dApp)
+                })
+                //burns
+                x.stateChanges.burns.forEach(y => {
+                    const index = stateUpdate.burns.findIndex(z => z.assetId === y.assetId)
+                    index !== -1 ? stateUpdate.burns[index].quantity += y.quantity : stateUpdate.burns.push({
+                        ...y,
+                        address: x.dApp
+                    })
                 }
+                )
+                //issues
+                x.stateChanges.issues.forEach(y => stateUpdate.issues.push({ ...y, address: x.dApp }))
+                //reissues
+                x.stateChanges.reissues.forEach(y => {
+                    const index = stateUpdate.reissues.findIndex(z => z.assetId === y.assetId)
+                    index !== -1 ? stateUpdate.reissues[index].quantity += y.quantity : stateUpdate.reissues.push({
+                        ...y,
+                        address: x.dApp
+                    })
+                }
+                )
+                //transfers
+                x.stateChanges.transfers.forEach(y => {
+                    const index = stateUpdate.transfers.findIndex(z => (z.asset === y.asset) && (z.address === y.address) && (x.dApp === z.sender))
+                    index !== -1
+                        ? stateUpdate.transfers[index].amount = (new BigNumber(stateUpdate.transfers[index].amount)).add(y.amount).toNumber()
+                        : stateUpdate.transfers.push({
+                            ...y,
+                            sender: x.dApp
+                        })
+                }
+                )
+                //sponsorFees
+                x.stateChanges.sponsorFees.forEach(y => {
+                    const index = stateUpdate.sponsorFees.findIndex(z => (z.assetId === y.assetId) && (z.address === x.dApp))
+                    index !== -1
+                        ? stateUpdate.sponsorFees[index] = { ...y, address: x.dApp }
+                        : stateUpdate.sponsorFees.push({ ...y, address: x.dApp })
+                }
+                )
+                //lease and leaseCancels
+                x.stateChanges.leases.forEach(y => stateUpdate.leases.push({ ...y, sender: x.dApp }))
+                x.stateChanges.leaseCancels.forEach(y => stateUpdate.leaseCancels.push({ ...y, address: x.dApp }))
+
+                recursiveFunction(x.stateChanges, x.dApp)
+            }
             )
         }
     }
